d69c90b68785224d2c6e0cc59d9062b0f979fedd
liuyaliu <308690763@qq.com>
2015-11-05 09:07:17 +0800

    smdemoinfo
    demo info

--- lylBBS/Doc/SQL/lylbbs.sql
+++ lylBBS/Doc/SQL/lylbbs.sql
@@ -1,7 +1,26 @@
 CREATE DATABASE 'YLBBS' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
-CREATE TABLE YL_USER_INFO;
-CREATE TABLE YL_LOGIN_LOG;
-CREATE TABLE YL_ROLE_INFO;
-CREATE TABLE YL_MENU_INFO;
-CREATE TABLE YL_AUTH_INFO;
+--用户基本信息表
+CREATE TABLE
+    sm_user_info
+    (
+        user_id bigint DEFAULT '0' NOT NULL,
+        user_name VARCHAR(40),
+        password VARCHAR(64),
+        user_type CHAR(1),
+        PRIMARY KEY (user_id)
+    )
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--用户操作日志表
+CREATE TABLE
+    sm_oper_log
+    (
+        oper_log_id bigint,
+        user_id bigint,
+        oper_type CHAR(1),
+        oper_time DATETIME,
+        oper_info VARCHAR(400)
+    )
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
